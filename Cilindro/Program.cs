ad6c792962f5426d79fea8199eb80fc22ce82a15
Micael Teixeira <mikatx2@hotmail.com>
2022-03-22 01:54:42 +0000

    ex4

---
+++ Cilindro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura;
+            float raio;
+            float pi = 3.1315926f;
+            float volume;
+
+            Console.Write("Altura:");
+            altura = float.Parse(Console.ReadLine());
+            
+            Console.Write("Raio:");
+            raio = float.Parse(Console.ReadLine());
+
+            volume = pi*(raio*raio)*altura;
+            Console.WriteLine(volume);
+
+            volume = 2*pi*raio*(raio+altura);
+            Console.WriteLine(volume);
+        }
+    }
+}
