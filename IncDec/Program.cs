9358b2f37561b34859ba58e2cc3dcd597872d615
Micael Teixeira <mikatx2@hotmail.com>
2022-03-22 01:37:10 +0000

    Ex1 done

---
+++ IncDec/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+
+            Console.WriteLine(x);
+            Console.WriteLine(x++);
+            Console.WriteLine(x);
+            
+            Console.WriteLine(y);
+            Console.WriteLine(--y);
+            Console.WriteLine(y);
+        }
+    }
+}
