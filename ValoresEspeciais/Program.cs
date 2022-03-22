edb69148063efb25fcd4ea60e25bc152849fe7e1
Micael Teixeira <mikatx2@hotmail.com>
2022-03-22 19:47:36 +0000

    Exercicio6 Valores Especiais done

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,30 @@ namespace ValoresEspeciais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int i = int.MaxValue;
+            float f = float.MaxValue;
+            decimal m = decimal.MaxValue;
+            double d = double.MaxValue;
+            short sh = short.MaxValue;
+            long lo = long.MaxValue;
+
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+            Console.WriteLine(m);
+            Console.WriteLine(d);
+            Console.WriteLine(sh);
+            Console.WriteLine(lo);
+
+            
+            
+            Console.WriteLine(float.PositiveInfinity);
+            Console.WriteLine(float.NegativeInfinity);
+            Console.WriteLine(float.NaN);
+
+            Console.WriteLine(double.PositiveInfinity);
+            Console.WriteLine(double.NegativeInfinity);
+            Console.WriteLine(double.NaN);
+
         }
     }
 }
