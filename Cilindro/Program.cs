e3ac33a0b649232aa058ccf7f572d1a0535f0fc0
Drackout <mikatx2@hotmail.com>
2025-02-27 15:48:01 +0000

    Ex5 pedir valores ao utilizador

---
+++ Cilindro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cilindro
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            double altura = double.Parse(Console.ReadLine());
+            double raio = double.Parse(Console.ReadLine());
+        }
+    }
+}
