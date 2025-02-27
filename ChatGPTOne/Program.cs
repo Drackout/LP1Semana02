96ed057f87b00c95fd8d6996a9b4ffd3b1d43f8b
Drackout <mikatx2@hotmail.com>
2025-02-27 17:03:30 +0000

    Finish exercicio 10

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -6,7 +6,41 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            string questao;
+            bool running = true;
+
+            do
+            {
+                Console.WriteLine("Faz-me uma Pergunta!");
+                questao = Console.ReadLine();
+
+                switch (questao)
+                {
+                    case "name":
+                        Console.WriteLine("My name is Optimus Prime!");
+                        break;
+
+                    case "year":
+                        Console.WriteLine("2025");
+                        break;
+
+                    case "banana":
+                        Console.WriteLine("BANANA!");
+                        break;
+
+                    case "hello":
+                        Console.WriteLine("Hello human");
+                        break;
+                        
+                    case "EXIT":
+                        running = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Não reconheço essa pergunta...");
+                        break;
+                }
+            } while (running);
         }
     }
 }
