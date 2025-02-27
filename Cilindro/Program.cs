2d5a6258101313fafea3dbcc724cbca71e9f8908
Drackout <mikatx2@hotmail.com>
2025-02-27 15:57:39 +0000

    Finish ex5

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,6 +8,10 @@ namespace Cilindro
         {
             double altura = double.Parse(Console.ReadLine());
             double raio = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{Math.PI*(Math.Pow(raio, 2f))*altura:f3}");
+
+            Console.WriteLine($"{2*Math.PI*raio*(raio+altura):f3}");
         }
     }
 }
