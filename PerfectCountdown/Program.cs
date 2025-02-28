d30416c6c9dd1b17b425b549757b32c23656c2b6
Drackout <mikatx2@hotmail.com>
2025-02-28 14:32:23 +0000

    Finish exercicio Avaliacao 28-2

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -6,7 +6,42 @@ namespace PerfectCountdown
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            bool running = true;
+            do
+            {
+                // Ask number
+                Console.Write("Start number: ");
+                int start = int.Parse(Console.ReadLine());
+                Console.Write("Step number: ");
+                int step = int.Parse(Console.ReadLine());
+
+                // Verifications
+                if(start<=0 || (step >1 && start < step))
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                    continue;
+                }
+                else if (step >= start)
+                {
+                    Console.WriteLine("Start must be higher than step. Try again.");
+                    continue;
+                }
+                else if (start % step != 0)
+                {
+                    Console.WriteLine("Start not divisible by step. Try again.");
+                    continue;
+                }
+                else
+                {
+                    // Countdown till 0
+                    do
+                    {
+                        Console.WriteLine(start);
+                        start -= step;
+                    } while (start >= 0);
+                    running = false;
+                }
+            } while (running);
         }
     }
 }
